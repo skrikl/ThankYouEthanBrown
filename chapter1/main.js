b4a45931fdaa853a0413dd39a5d06f42b8839626
skrikl <sergey.kriklivyy@gmail.com>
2019-05-10 11:46:03 +0300

    PaperJS draw example

--- chapter1/main.js
+++ chapter1/main.js
@@ -3,7 +3,13 @@ $(document).ready(function() {
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
 
-    //TODO
+    var c;
+    for(var x=25; x<400; x+=50) {
+        for(var y=25; y<400; y+=50) {
+            c = Shape.Circle(x, y, 20);
+            c.fillColor = 'lime';
+        }
+    }
 
     paper.view.draw();
 });
